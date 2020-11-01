69cf3e73d62d67050d418cb2b857e3387316b7b5
Harsh Dhorajia <harshdhorajiya1304@gmail.com>
2020-11-01 15:21:46 +0530

    Userlist bug

--- src/Components/SongList.js
+++ src/Components/SongList.js
@@ -9,54 +9,136 @@ import TableHead from '@material-ui/core/TableHead';
 import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
 
-const columns = [
-  { id: 'name', label: 'Name', minWidth: 170 },
-  { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
+const column = [
   {
-    id: 'population',
-    label: 'Population',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toLocaleString('en-US'),
+    id:"user",
+    label:"UserName",
+    // format: (value) => value.toLocaleString('en-US'),
+
   },
   {
-    id: 'size',
-    label: 'Size\u00a0(km\u00b2)',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toLocaleString('en-US'),
+    id:"email",
+    label:"Email",
+    // format: (value) => value.toLocaleString('en-US'),
+
   },
   {
-    id: 'density',
-    label: 'Density',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toFixed(2),
+    id:"password",
+    label:"Password",
+    // format: (value) => value.toLocaleString('en-US'),
+
+  },
+  {
+    id:"created At",
+    label: "Created At",
+    // format: (value) => value.toLocaleString('en-US'),
   },
 ];
 
-function createData(name, code, population, size) {
-  const density = population / size;
-  return { name, code, population, size, density };
-}
+const row = [
+  {
 
-const rows = [
-  createData('India', 'IN', 1324171354, 3287263),
-  createData('China', 'CN', 1403500365, 9596961),
-  createData('Italy', 'IT', 60483973, 301340),
-  createData('United States', 'US', 327167434, 9833520),
-  createData('Canada', 'CA', 37602103, 9984670),
-  createData('Australia', 'AU', 25475400, 7692024),
-  createData('Germany', 'DE', 83019200, 357578),
-  createData('Ireland', 'IE', 4857000, 70273),
-  createData('Mexico', 'MX', 126577691, 1972550),
-  createData('Japan', 'JP', 126317000, 377973),
-  createData('France', 'FR', 67022000, 640679),
-  createData('United Kingdom', 'GB', 67545757, 242495),
-  createData('Russia', 'RU', 146793744, 17098246),
-  createData('Nigeria', 'NG', 200962417, 923768),
-  createData('Brazil', 'BR', 210147125, 8515767),
-];
+    id: 1,
+    username: "Harsh",
+    email: "Hatr@gata.aahla",
+    password: "2235555",
+    createdAt: "01-11-2020",
+  },
+  {
+    id: 2,
+    username: "Harsh",
+    email: "Hatr@gata.aahla",
+    password: "2235555",
+    createdAt: "01-11-2020",
+  },
+  {
+    id: 3,
+    username: "Harsh",
+    email: "Hatr@gata.aahla",
+    password: "2235555",
+    createdAt: "01-11-2020",
+  },
+  {
+    id: 4,
+    username: "Harsh",
+    email: "Hatr@gata.aahla",
+    password: "2235555",
+    createdAt: "01-11-2020",
+  },
+  {
+    id: 5,
+    username: "Harsh",
+    email: "Hatr@gata.aahla",
+    password: "2235555",
+    createdAt: "01-11-2020",
+  },
+  {
+    id: 6,
+    username: "Harsh",
+    email: "Hatr@gata.aahla",
+    password: "2235555",
+    createdAt: "01-11-2020",
+  },
+  {
+    id: 7,
+    username: "Harsh",
+    email: "Hatr@gata.aahla",
+    password: "2235555",
+    createdAt: "01-11-2020",
+  },
+  {
+    id: 8,
+    username: "Harsh",
+    email: "Hatr@gata.aahla",
+    password: "2235555",
+    createdAt: "01-11-2020",
+  },
+  {
+    id: 9,
+    username: "Harsh",
+    email: "Hatr@gata.aahla",
+    password: "2235555",
+    createdAt: "01-11-2020",
+  },
+  {
+    id: 10,
+    username: "Harsh",
+    email: "Hatr@gata.aahla",
+    password: "2235555",
+    createdAt: "01-11-2020",
+  },
+  {
+    id: 11,
+    username: "Harsh",
+    email: "Hatr@gata.aahla",
+    password: "2235555",
+    createdAt: "01-11-2020",
+  },
+
+]
+// function createData(name, code, population, size) {
+//   const density = population / size;
+//   return { name, code, population, size, density };
+// }
+
+// const rows = [
+//   {'India', 'IN', 1324171354, 3287263}
+//   'India', 'IN', 1324171354, 3287263,
+//   'China', 'CN', 1403500365, 9596961,
+//   'Italy', 'IT', 60483973, 301340,
+//   'United States', 'US', 327167434, 9833520,
+//   'Canada', 'CA', 37602103, 9984670,
+//   'Australia', 'AU', 25475400, 7692024,
+//   'Germany', 'DE', 83019200, 357578,
+//   'Ireland', 'IE', 4857000, 70273,
+//   'Mexico', 'MX', 126577691, 1972550,
+//   'Japan', 'JP', 126317000, 377973,
+//   'France', 'FR', 67022000, 640679,
+//   'United Kingdom', 'GB', 67545757, 242495,
+//   'Russia', 'RU', 146793744, 17098246,
+//   'Nigeria', 'NG', 200962417, 923768,
+//   'Brazil', 'BR', 210147125, 8515767,
+// ];
 
 const useStyles = makeStyles({
   root: {
@@ -67,7 +149,7 @@ const useStyles = makeStyles({
   },
 });
 
-export default function SongList  () {
+export default function SongList() {
   const classes = useStyles();
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
@@ -84,29 +166,30 @@ export default function SongList  () {
   return (
     <Paper className={classes.root}>
       <TableContainer className={classes.container}>
-        <Table stickyHeader aria-label="sticky table">
+        <Table stickyHeader aria-email="sticky table">
           <TableHead>
             <TableRow>
-              {columns.map((column) => (
-                <TableCell
-                  key={column.id}
-                  align={column.align}
-                  style={{ minWidth: column.minWidth }}
-                >
-                  {column.label}
-                </TableCell>
-              ))}
+              {
+                column.map((column) => {
+                  return (
+                    <TableCell key={column}>
+                      {column.label}
+                    </TableCell>
+                  )
+                })
+              }
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
+            {row.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
               return (
                 <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                  {columns.map((column) => {
-                    const value = row[column.id];
-                    return (
-                      <TableCell key={column.id} align={column.align}>
-                        {column.format && typeof value === 'number' ? column.format(value) : value}
+                  {column.map((column) => {
+                    const value = row[column.id]
+                     return (
+                      <TableCell key={column.id}>
+                        {value}
+                        {/* {column.format && typeof value === 'number' ? column.format(value) : value} */}
                       </TableCell>
                     );
                   })}
@@ -119,7 +202,7 @@ export default function SongList  () {
       <TablePagination
         rowsPerPageOptions={[10, 25, 100]}
         component="div"
-        count={rows.length}
+        count={row.length}
         rowsPerPage={rowsPerPage}
         page={page}
         onChangePage={handleChangePage}
