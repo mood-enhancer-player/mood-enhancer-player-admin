49c5dd5fa6e33a41cc77e395e0a4bcb0aa9252e5
ShubhamKhunt <shubhamkhunt08>
2020-11-01 19:26:29 +0530

    UserList query complete

--- src/Components/UserList.js
+++ src/Components/UserList.js
@@ -9,7 +9,9 @@ import {
   TableCell,
   Paper,
   TablePagination,
+  CircularProgress,
 } from "@material-ui/core";
+import { useQuery, gql } from "@apollo/client";
 
 const useStyles = makeStyles({
   root: {
@@ -110,8 +112,11 @@ const UserList = () => {
   const classes = useStyles();
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [tableIndex, setTableIndex] = useState(0);
 
-  const columTitleRow = ["Id", "Username", "E-mail", "Password", "createAt"];
+  const columTitleRow = ["Id", "Username", "E-mail", "createAt"];
+
+  const { loading, data, error } = useQuery(USERLIST_QUERY);
 
   const onChangePage = (e, nextPage) => {
     setPage(nextPage);
@@ -121,56 +126,72 @@ const UserList = () => {
     setRowsPerPage(e.target.value);
     setPage(0);
   };
+
   return (
-    <Paper className={classes.root}>
-      <TableContainer className={classes.container}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              {columTitleRow.map((title) => (
-                <TableCell
-                  key={title}
-                  className={`${classes.textAlign} ${classes.columTitleRow}`}
-                >
-                  {title}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {users
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((user) => (
-                <TableRow key={user.id} hover>
-                  <TableCell className={classes.textAlign}>{user.id}</TableCell>
-                  <TableCell className={classes.textAlign}>
-                    {user.username}
-                  </TableCell>
-                  <TableCell className={classes.textAlign}>
-                    {user.email}
-                  </TableCell>
-                  <TableCell className={classes.textAlign}>
-                    {user.password}
-                  </TableCell>
-                  <TableCell className={classes.textAlign}>
-                    {user.createdAt}
-                  </TableCell>
+    <>
+      {loading || !data ? (
+        <CircularProgress />
+      ) : (
+        <Paper className={classes.root}>
+          <TableContainer className={classes.container}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  {columTitleRow.map((title) => (
+                    <TableCell
+                      key={title}
+                      className={`${classes.textAlign} ${classes.columTitleRow}`}
+                    >
+                      {title}
+                    </TableCell>
+                  ))}
                 </TableRow>
-              ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        component="div"
-        rowsPerPageOptions={[5, 10, 15, 20]}
-        count={users.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onChangePage={onChangePage}
-        onChangeRowsPerPage={onChangeRowsPerPage}
-      />
-    </Paper>
+              </TableHead>
+              <TableBody>
+                {data.getAllUsers
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map((user, index) => (
+                    <TableRow key={user.id} hover>
+                      <TableCell className={classes.textAlign}>
+                        {index + page * 5 + 1}
+                      </TableCell>
+                      <TableCell className={classes.textAlign}>
+                        {user.username}
+                      </TableCell>
+                      <TableCell className={classes.textAlign}>
+                        {user.email}
+                      </TableCell>
+                      <TableCell className={classes.textAlign}>
+                        {new Date(Number(user.createdAt)).toDateString()}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <TablePagination
+            component="div"
+            rowsPerPageOptions={[5, 10, 15, 20]}
+            count={users.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onChangePage={onChangePage}
+            onChangeRowsPerPage={onChangeRowsPerPage}
+          />
+        </Paper>
+      )}
+    </>
   );
 };
+
+const USERLIST_QUERY = gql`
+  query {
+    getAllUsers {
+      email
+      username
+      createdAt
+    }
+  }
+`;
 
 export default UserList;
